64122ffea7dbcb9661fe67a35f0ebd340315eb0a
Lucsun <suntianxiang@authing.cn>
2022-05-17 14:55:57 +0800

    update React version

--- src/Guard/index.tsx
+++ src/Guard/index.tsx
@@ -1,39 +1,19 @@
-import React, { useState } from "react";
-import { AuthenticationClient, Guard } from "@authing/react-ui-components";
+import React from "react";
+import { Guard } from "@authing/react-ui-components";
 // 引入 css 文件
 import "@authing/react-ui-components/lib/index.min.css";
 
 const GuardDemo = (props) => {
+  // 替换你的 AppId
   const appId = "5d70d0e991fdd597019df70d";
 
   const { updateUser } = props;
 
-  const [client, setClient] = useState<AuthenticationClient>();
-
-  const onLogin = (user, client) => {
+  const onLogin = (user) => {
     updateUser(user);
-
-    console.log(user);
-
-    setClient(client);
   };
 
-  const onLogout = async () => {
-    // await client.logout()
-
-    const user = await client.getCurrentUser()
-
-    console.log(user)
-
-    updateUser(null);
-  }
-
-  return (
-    <>
-      <Guard appId={appId} onLogin={onLogin} />
-      {client && <button  className='btn' onClick={onLogout}>登出</button>}
-    </>
-  );
-};
+  return <Guard appId={appId} onLogin={onLogin} />;
+}; 
 
 export default GuardDemo;
